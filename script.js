d5f05209e04457ea5f7964deacca0ea0180ca098
Yan <bambagaevdmitij@gmail.com>
2021-10-28 03:15:52 +0800

    practice 2

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const nomto = 'cool';
+
+switch (nomto) {
+    case 'govno':
+        console.log('youre poopman');
+        break;
+    case 'cool':
+        console.log('youre still poopman');
+        break;
+    case '':
+        console.log('nothing');
+        break;
+}
